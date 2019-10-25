82312a0237f85210e3204a5a695d9e676be13389
Darby Lim <thlim@robotis.com>
2019-10-25 18:00:51 +0900

    Added sets

--- a-star/sketch.js
+++ a-star/sketch.js
@@ -2,20 +2,105 @@ var cols = 5;
 var rows = 5;
 var grid = new Array(cols);
 
+var openSet = [];
+var closedSet = [];
+var start;
+var end;
 
-function setup() {
+var w, h;
+
+function Spot(i, j)
+{
+  this.x = i;
+  this.y = j;
+
+  this.f = 0;
+  this.g = 0;
+  this.h = 0;
+
+  this.show = function(color)
+  {
+    fill(color);
+    noStroke();
+    rect(this.x * w, this.y * h, w-1, h-1); 
+  }
+}
+
+function setup() 
+{
   createCanvas(400, 400);
   console.log('A*');
 
-  for (var i = 0; i < cols; i++)
+  w = width / cols;
+  h = height / rows;
+
+  // Making a 2D array
+  for (var i = 0; i < cols; i++) 
   {
     grid[i] = new Array(rows);
   }
 
-  consol.log('grid');
+  for (var i = 0; i < cols; i++) 
+  {
+    for (var j = 0; j < rows; j++)
+    {
+      grid[i][j] = new Spot(i, j);
+    }
+  }
+
+  start = grid[0][0];
+  end = grid[cols - 1][rows -1];
+
+  openSet.push(start);
+
+  console.log(grid);
 }
 
-function draw() {
+function draw() 
+{
+  if (openSet.length > 0)
+  {
+    var winner = 0;
+    for (var i = 0; i < openSet.length; i++)
+    {
+      if (openSet[i].f < openSet[winner].f)
+      {
+        winner = i;
+      }
+    }
+
+    var current = openSet[winner];
+
+    if (current == end)
+    {
+      console.log("DONE!");
+    }
+
+    closedSet.push(current);
+    // we can keep going
+  }
+  else
+  {
+    // no solution
+  }
+
   background(0);
-  
+
+  for (var i = 0; i < cols; i++)
+  {
+    for (var j = 0; j < rows; j++)
+    {
+      grid[i][j].show(color(255));
+    }
+  }
+
+  for (var i = 0; i < closedSet.length; i++)
+  {
+    closedSet[i].show(color(255, 0, 0));
+  }
+
+  for (var i = 0; i < openSet.length; i++)
+  {
+    openSet[i].show(color(0, 255, 0));
+  }
 }
