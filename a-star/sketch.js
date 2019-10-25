808af7a5b8f23f0fbc4565ba955e90ff5d87c70f
Darby Lim <thlim@robotis.com>
2019-10-25 15:33:38 +0900

    Added array

--- a-star/sketch.js
+++ a-star/sketch.js
@@ -1,27 +1,21 @@
+var cols = 5;
+var rows = 5;
+var grid = new Array(cols);
+
+
 function setup() {
-  createCanvas(640, 360);
-//  print("setup function!");
+  createCanvas(400, 400);
+  console.log('A*');
+
+  for (var i = 0; i < cols; i++)
+  {
+    grid[i] = new Array(rows);
+  }
+
+  consol.log('grid');
 }
 
 function draw() {
-  background(255, 255, 0);
-  ellipseMode(CENTER);
-  rectMode(CENTER);
-  
-  // Body
-  fill(255, 0, 0);
-  rect(240, 145, 20, 100);
-  
-  // Head
-  ellipse(240, 115, 60, 60);
-  
-  // Eyes
-  fill(0, 255, 0);
-  ellipse(221, 115, 16, 32);
-  ellipse(259, 115, 16, 32);
-  
-  // Legs
-  line(230, 195, 220, 205);
-  line(250, 195, 260, 205);
+  background(0);
   
 }
