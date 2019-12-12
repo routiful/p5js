03fe5e486098181bd5a8ab0825db092a3eafae22
Darby Lim <thlim@robotis.com>
2019-12-12 18:06:26 +0900

    Added dw

--- dwa/sketch.js
+++ dwa/sketch.js
@@ -35,40 +35,58 @@ class DWA
     this.clearance_cost_gain = clearance_cost_gain;
   }
 
-  motion_predict(x, u, dt)
+  motion_predict(state, input, dt)
   {
-    // x[] : x_pos, y_pos, theta, lin_vel, ang_vel
-    // u[] : lin_vel, ang_vel
+    // state[] : x_pos, y_pos, theta, lin_vel, ang_vel
+    // input[] : lin_vel, ang_vel
     // dt : control period time
 
-    x[2] += u[1] * dt;
+    state[2] += input[1] * dt;
 
-    x[0] += u[0] * dt * cos(x[2]);
-    x[1] += u[0] * dt * sin(x[2]);
+    state[0] += input[0] * dt * cos(state[2]);
+    state[1] += input[0] * dt * sin(state[2]);
 
-    x[3] = u[0];
-    x[4] = u[1];
+    state[3] = input[0];
+    state[4] = input[1];
 
-    return x;
+    return state;
   }
 
-  update_dynamic_window(x)
+  update_dynamic_window(state, scan_data)
   {
     // update possible velocity
     let Vs = [this.min_lin_vel, this.max_lin_vel, this.min_ang_vel, this.max_ang_vel];
 
     // update admissible velocity
+    let Va = [];
+    for (let i = 0; i < scan_data.length; i++)
+    {
+      Va.push([
+        this.min_lin_vel,
+        sqrt(2 * scan_data[i] * this.limit_lin_acc),
+        this.min_ang_vel,
+        sqrt(2 * scan_data[i] * this.limit_ang_acc)]);
+    }
+
+    // update dynamic window velocity
     let Vd = [
-      x[3] - this.limit_lin_acc * this.dt,
-      x[3] + this.limit_lin_acc * this.dt,
-      x[4] - this.limit_ang_acc * this.dt,
-      x[4] + this.limit_ang_acc * this.dt];
+      state[3] - this.limit_lin_acc * this.dt,
+      state[3] + this.limit_lin_acc * this.dt,
+      state[4] - this.limit_ang_acc * this.dt,
+      state[4] + this.limit_ang_acc * this.dt];
 
-    // [vmin,vmax, yaw_rate min, yaw_rate max]
-    this.dw = [max(Vs[0], Vd[0]), min(Vs[1], Vd[1]),
-          max(Vs[2], Vd[2]), min(Vs[3], Vd[3])]
+    // update resulting velocity
+    let Vr = [];
+    for (let i = 0; i < scan_data.length; i++)
+    {
+      Vr.push([
+      max(Vd[0], max(Vs[0], Va[i][0])),
+      min(Vd[1], min(Vs[1], Va[i][1])),
+      max(Vd[2], max(Vs[2], Va[i][2])),
+      max(Vd[3], max(Vs[3], Va[i][3]))]);
+    }
 
-    return dw
+    return Vr;
   }
 
   heading()
@@ -133,6 +151,7 @@ let ang_acc = 0.0;
 
 let robot_state = [x_in, y_in, theta_in, lin_vel, ang_vel];
 let goal_pose = {x: 0.0, y: 0.0};
+let result = [];
 
 function setup()
 {
@@ -176,10 +195,12 @@ function draw()
       obstacles[i].show();
     }
 
-    robot_state = dwa.motion_predict(robot_state, [lin_vel, ang_vel], dt);
 
     robot.odom_update(lin_vel, ang_vel, lin_acc, ang_acc, dt);
     robot.scan_update(obstacles);
+
+    robot_state = dwa.motion_predict(robot_state, [lin_vel, ang_vel], dt);
+    result = dwa.update_dynamic_window(robot_state, robot.scan_data);
     robot.draw();
 
     noStroke();
