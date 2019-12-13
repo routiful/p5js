067d4395653a359081a81d0a47f718d4a9e900d0
Darby Lim <thlim@robotis.com>
2019-12-13 17:32:53 +0900

    Updated goal pose

--- dwa/sketch.js
+++ dwa/sketch.js
@@ -1,115 +1,3 @@
-class DWA
-{
-  constructor(
-    max_lin_vel,
-    min_lin_vel,
-    max_ang_vel,
-    min_ang_vel,
-    limit_lin_acc,
-    limit_ang_acc,
-    v_reso,
-    yawrate_reso,
-    dt,
-    predict_time,
-    heading_cost_gain,
-    velocity_cost_gain,
-    clearance_cost_gain)
-  {
-    this.max_lin_vel = max_lin_vel;
-    this.min_lin_vel = min_lin_vel;
-
-    this.max_ang_vel = max_ang_vel;
-    this.min_ang_vel = min_ang_vel;
-
-    this.limit_lin_acc = limit_lin_acc;
-    this.limit_ang_acc = limit_ang_acc;
-
-    this.v_reso = v_reso;
-    this.yawrate_reso = yawrate_reso;
-
-    this.dt = dt;
-    this.predict_time = predict_time;
-
-    this.heading_cost_gain = heading_cost_gain;
-    this.velocity_cost_gain = velocity_cost_gain;
-    this.clearance_cost_gain = clearance_cost_gain;
-  }
-
-  motion_predict(state, input, dt)
-  {
-    // state[] : x_pos, y_pos, theta, lin_vel, ang_vel
-    // input[] : lin_vel, ang_vel
-    // dt : control period time
-
-    state[2] += input[1] * dt;
-
-    state[0] += input[0] * dt * cos(state[2]);
-    state[1] += input[0] * dt * sin(state[2]);
-
-    state[3] = input[0];
-    state[4] = input[1];
-
-    return state;
-  }
-
-  update_dynamic_window(state, scan_data)
-  {
-    // update possible velocity
-    let Vs = [this.min_lin_vel, this.max_lin_vel, this.min_ang_vel, this.max_ang_vel];
-
-    // update admissible velocity
-    let Va = [];
-    for (let i = 0; i < scan_data.length; i++)
-    {
-      Va.push([
-        this.min_lin_vel,
-        sqrt(2 * scan_data[i] * this.limit_lin_acc),
-        this.min_ang_vel,
-        sqrt(2 * scan_data[i] * this.limit_ang_acc)]);
-    }
-
-    // update dynamic window velocity
-    let Vd = [
-      state[3] - this.limit_lin_acc * this.dt,
-      state[3] + this.limit_lin_acc * this.dt,
-      state[4] - this.limit_ang_acc * this.dt,
-      state[4] + this.limit_ang_acc * this.dt];
-
-    // update resulting velocity
-    let Vr = [];
-    for (let i = 0; i < scan_data.length; i++)
-    {
-      Vr.push([
-      max(Vd[0], max(Vs[0], Va[i][0])),
-      min(Vd[1], min(Vs[1], Va[i][1])),
-      max(Vd[2], max(Vs[2], Va[i][2])),
-      max(Vd[3], max(Vs[3], Va[i][3]))]);
-    }
-
-    return Vr;
-  }
-
-  calc_input(x, Vr, goal_pose, scan_data)
-  {
-
-  }
-
-  heading()
-  {
-
-  }
-
-  clearance()
-  {
-
-  }
-
-  velocity()
-  {
-
-  }
-}
-
 // robot config
 let robot_radius = 10.0;
 
@@ -147,16 +35,19 @@ let robot;
 let obstacles = [];
 let dwa;
 
+let mouse_pressed_cnt = 0;
 let t = 0;
 
 let lin_vel = 0.0;
 let ang_vel = 0.0;
+
 let lin_acc = 0.0;
 let ang_acc = 0.0;
 
-let robot_state = [x_in, y_in, theta_in, lin_vel, ang_vel];
-let goal_pose = {x: 0.0, y: 0.0};
-let result = [];
+let predicted_robot_state = [x_in, y_in, theta_in, lin_vel, ang_vel];
+let goal_pose = [];
+let dynamic_window = [];
+let goal_trajectory = [];
 
 function setup()
 {
@@ -193,29 +84,42 @@ function draw()
   if (millis() - t > dt * 1000)
   {
     axis.show(width, height);
-    ellipse(goal_pose.x, goal_pose.y, 5, 5);
+
+    stroke(0);
+    strokeWeight(1);
+    ellipse(goal_pose[0], goal_pose[1], 5, 5);
+    line(goal_pose[0], goal_pose[1], goal_pose[2], goal_pose[3]);
 
     for (let i = 0; i < obstacles.length; i++)
     {
       obstacles[i].show();
     }
 
-
     robot.odom_update(lin_vel, ang_vel, lin_acc, ang_acc, dt);
     robot.scan_update(obstacles);
 
-    robot_state = dwa.motion_predict(robot_state, [lin_vel, ang_vel], dt);
-    result = dwa.update_dynamic_window(robot_state, robot.scan_data);
+    predicted_robot_state = dwa.motion_predict(predicted_robot_state, [lin_vel, ang_vel], dt);
+    dwa.update_dynamic_window(predicted_robot_state, robot.scan_data);
+
+
     robot.draw();
 
     noStroke();
     textSize(8);
-    text('x : ' + robot.x, width - 80, height - 40);
-    text('y : ' + robot.y, width - 80, height - 30);
-    text('theta : ' + robot.theta, width - 80, height - 20);
+    text('lin_vel : ' + robot.lin_vel, width - 60, height - 130);
+    text('ang_vel : ' + robot.ang_vel, width - 60, height - 120);
 
-    text('lin_vel : ' + robot.lin_vel, width - 80, height - 60);
-    text('ang_vel : ' + robot.ang_vel, width - 80, height - 50);
+    text(' ', width - 60, height - 110);
+    text('[current]', width - 60, height - 100);
+    text('x : ' + robot.x, width - 60, height - 90);
+    text('y : ' + robot.y, width - 60, height - 80);
+    text('theta : ' + robot.theta, width - 60, height - 70);
+
+    text(' ', width - 60, height - 60);
+    text('[target]', width - 60, height - 50);
+    text('x : ' + goal_pose[0], width - 60, height - 40);
+    text('y : ' + goal_pose[1], width - 60, height - 30);
+    text('theta : ' + goal_pose[4], width - 60, height - 20);
 
     t = millis();
   }
@@ -251,14 +155,31 @@ function keyPressed()
 
 function mousePressed()
 {
-  noLoop();
+  if (mouse_pressed_cnt == 0)
+  {
+    noLoop();
+    goal_pose = [];
+    goal_pose.push(mouseX);
+    goal_pose.push(mouseY);
+    mouse_pressed_cnt++;
+  }
+  else if (mouse_pressed_cnt == 1)
+  {
+    goal_pose.push(mouseX);
+    goal_pose.push(mouseY);
+    goal_pose.push(atan2(goal_pose[3] - goal_pose[1], goal_pose[2] - goal_pose[0]));
 
-  goal_pose = {x: mouseX, y: mouseY};
-  robot = new Robot(robot_radius, x_in, y_in, theta_in, scan_range, scan_offset, scan_dist);
-  lin_vel = 0.0;
-  ang_vel = 0.0;
+    robot = new Robot(robot_radius, x_in, y_in, theta_in, scan_range, scan_offset, scan_dist);
 
-  robot_state = [x_in, y_in, theta_in, lin_vel, ang_vel];
+    lin_vel = 0.0;
+    ang_vel = 0.0;
+    lin_acc = 0.0;
+    ang_acc = 0.0;
 
-  loop();
+    predicted_robot_state = [x_in, y_in, theta_in, lin_vel, ang_vel];
+
+    mouse_pressed_cnt = 0;
+
+    loop();
+  }
 }
