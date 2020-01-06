81b8df8c99945a34e91bca2886289de42240433d
Darby Lim <thlim@robotis.com>
2020-01-06 16:47:35 +0900

    Updated clearance

--- dwa/dwa.js
+++ dwa/dwa.js
@@ -33,8 +33,6 @@ class DWA
     this.heading_bias = heading_bias;
     this.velocity_bias = velocity_bias;
     this.clearance_bias = clearance_bias;
-
-    // this.all_computed_trajectory = [];
   }
 
   predict_motion(state, input, dt)
@@ -44,7 +42,7 @@ class DWA
 
     let get_state = state.slice(); // Deep copy
 
-    let epsilon = 0.000000001;
+    let epsilon = 0.000001;
     if (input[0] - get_state[3] > epsilon)
     {
       get_state[3] = get_state[3] + this.limit_lin_acc * dt;
@@ -58,6 +56,11 @@ class DWA
       get_state[3] = input[0];
     }
 
+    if (abs(this.lin_vel) < epsilon)
+    {
+      this.lin_vel = 0.0;
+    }
+
     if (input[1] - get_state[4] > epsilon)
     {
       get_state[4] = get_state[4] + this.limit_ang_acc * dt;
@@ -69,6 +72,11 @@ class DWA
     else
     {
       get_state[4] = input[1];
+    }
+
+    if (abs(this.ang_vel) < epsilon)
+    {
+      this.ang_vel = 0.0;
     }
 
     let delta_s = get_state[3] * dt;
@@ -115,7 +123,7 @@ class DWA
     return Vr;
   }
 
-  maximizing_objective_function(state, Vr, goal_pose, robot_radius, scan_data, scan_dist)
+  maximizing_objective_function(state, Vr, goal_pose, robot_radius, obstacles, scan_range, scan_offset, scan_dist)
   {
     let lin_vel = 0.0;
     let ang_vel = 0.0;
@@ -142,7 +150,13 @@ class DWA
 
         heading_cost = this.heading_bias * this.heading(predicted_trajectory, goal_pose);
         velocity_cost = this.velocity_bias * this.velocity(predicted_trajectory);
-        clearance_cost = this.clearance_bias * this.clearance(predicted_trajectory, robot_radius, scan_data, scan_dist);
+        clearance_cost = this.clearance_bias * this.clearance(
+          predicted_trajectory,
+          robot_radius,
+          obstacles,
+          scan_range,
+          scan_offset,
+          scan_dist);
 
         let cost_sum = heading_cost + velocity_cost + clearance_cost;
 
@@ -238,11 +252,193 @@ class DWA
     return cost;
   }
 
-  clearance(trajectory, robot_radius, scan_data, scan_dist)
+  clearance(trajectory, robot_radius, obstacles, scan_range, scan_offset, scan_dist)
   {
-    let min_scan_data = min(scan_data);
-    let cost = map(min_scan_data, robot_radius * 1.5, scan_dist, 0.0, 1.0);
+    let x = trajectory[trajectory.length - 1][0];
+    let y = trajectory[trajectory.length - 1][1];
+    let theta = trajectory[trajectory.length - 1][2];
 
-    return 1 / cost;
+    this.check_inside = function()
+    {
+      // ray-casting algorithm based on
+      // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+
+      // var x = point[0], y = point[1];
+      let vs = obstacles;
+      let inside = false;
+
+      for (let i = 0, j = vs.length - 1; i < vs.length; j = i++)
+      {
+        let xi = vs[i][0], yi = vs[i][1];
+        let xj = vs[j][0], yj = vs[j][1];
+
+        let intersect = ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect)
+        {
+          inside = !inside;
+        }
+      }
+      return inside;
+    }
+
+    // ref) https://www.openprocessing.org/sketch/135314/
+    this.line_intersection = function(x1, y1, x2, y2, x3, y3, x4, y4)
+    {
+      let bx = x2 - x1;
+      let by = y2 - y1;
+      let dx = x4 - x3;
+      let dy = y4 - y3;
+
+      let b_dot_d_perp = bx * dy - by * dx;
+
+      if (b_dot_d_perp == 0) return scan_dist;
+
+      let cx = x3 - x1;
+      let cy = y3 - y1;
+
+      let t = (cx * dy - cy * dx) / b_dot_d_perp;
+      if (t < 0 || t > 1) return scan_dist;
+
+      let u = (cx * by - cy * bx) / b_dot_d_perp;
+      if (u < 0 || u > 1) return scan_dist;
+
+      let intersection_point_x = x1+t*bx;
+      let intersection_point_y = y1+t*by;
+
+      return sqrt(
+        pow((x - intersection_point_x), 2) +
+        pow((y - intersection_point_y), 2));
+    }
+
+    this.predict_scan_data = function()
+    {
+      let cnt = 0;
+      let scan_data = [];
+
+      for (let i = scan_range[0]; i < scan_range[1]; i += scan_offset)
+      {
+        let min_obstacle_dist = [];
+        for (let j = 0; j < obstacles.length; j++)
+        {
+          let obstacle_dist = [];
+          obstacle_dist.push(
+            this.line_intersection(
+              x,
+              y,
+              x + cos(theta + i) * scan_dist,
+              y + sin(theta + i) * scan_dist,
+              obstacles[j].x1,
+              obstacles[j].y1,
+              obstacles[j].x2,
+              obstacles[j].y2
+            ));
+
+          obstacle_dist.push(
+            this.line_intersection(
+              x,
+              y,
+              x + cos(theta + i) * scan_dist,
+              y + sin(theta + i) * scan_dist,
+              obstacles[j].x2,
+              obstacles[j].y2,
+              obstacles[j].x3,
+              obstacles[j].y3
+            ));
+
+          obstacle_dist.push(
+            this.line_intersection(
+              x,
+              y,
+              x + cos(theta + i) * scan_dist,
+              y + sin(theta + i) * scan_dist,
+              obstacles[j].x3,
+              obstacles[j].y3,
+              obstacles[j].x4,
+              obstacles[j].y4
+            ));
+
+          obstacle_dist.push(
+            this.line_intersection(
+              x,
+              y,
+              x + cos(theta + i) * scan_dist,
+              y + sin(theta + i) * scan_dist,
+              obstacles[j].x4,
+              obstacles[j].y4,
+              obstacles[j].x1,
+              obstacles[j].y1
+            ));
+
+          min_obstacle_dist[j] = min(obstacle_dist);
+        }
+
+        min_obstacle_dist.push(
+          this.line_intersection(
+            x,
+            y,
+            x + cos(theta + i) * scan_dist,
+            y + sin(theta + i) * scan_dist,
+            0.0,
+            0.0,
+            width,
+            0.0
+          ));
+
+        min_obstacle_dist.push(
+          this.line_intersection(
+            x,
+            y,
+            x + cos(theta + i) * scan_dist,
+            y + sin(theta + i) * scan_dist,
+            0.0,
+            0.0,
+            0.0,
+            height
+          ));
+
+        min_obstacle_dist.push(
+          this.line_intersection(
+            x,
+            y,
+            x + cos(theta + i) * scan_dist,
+            y + sin(theta + i) * scan_dist,
+            width,
+            0.0,
+            width,
+            height
+          ));
+
+
+        min_obstacle_dist.push(
+          this.line_intersection(
+            x,
+            y,
+            x + cos(theta + i) * scan_dist,
+            y + sin(theta + i) * scan_dist,
+            0.0,
+            height,
+            width,
+            height
+          ));
+
+        scan_data[cnt] = min(min_obstacle_dist);
+        cnt++;
+      }
+      return scan_data;
+    }
+
+    if (this.check_inside() == false)
+    {
+      let predicted_scan_data = this.predict_scan_data();
+
+      let min_scan_data = min(predicted_scan_data);
+      let cost = map(min_scan_data, robot_radius * 1.0, scan_dist, 0.0, 1.0);
+
+      return cost;
+    }
+    else
+    {
+      return 0.0;
+    }
   }
 }
